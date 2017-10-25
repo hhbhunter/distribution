9e2ae407636e1c6e130cd8dfd31019e2a478f7bf
hhbhunter <houhuibin@58ganji.com>
2017-10-25 14:47:41 +0800

    test control code

---
+++ src/test/java/stp/roletest/StartControl.java
@@ -0,0 +1,41 @@
+package stp.roletest;
+
+import java.util.concurrent.TimeUnit;
+
+import com.stp.distribution.entity.ZkTask;
+import com.stp.distribution.user.TaskCache;
+import com.stp.distribution.user.TaskCreaterService;
+import com.stp.distribution.user.TaskResults;
+
+public class StartControl {
+	public static void main(String[] args) {
+		new TaskCreaterService(new TaskResults() {
+
+			@Override
+			public void updateDB(ZkTask task) {
+				// TODO Auto-generated method stub
+				System.out.println("control update task id="+task.getTaskid()+" stat="+task.getStat());
+			}
+		}).init();
+		int id=1;
+		while(true){
+			try {
+				ZkTask task=new ZkTask();
+				task.setStartCmd("you start cmd");
+				task.setStopCmd("you stop cmd");
+				task.setCmdPath("you work path");
+				task.setExeNum(1);
+				task.setStat("create");
+				task.setTaskid(id);
+				task.setType("PERFORME");
+				//				task.setType("AUTO");
+				TaskCache.addTask(task);
+				TimeUnit.MINUTES.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
